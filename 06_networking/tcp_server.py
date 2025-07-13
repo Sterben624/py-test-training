546c04d367c4679e8e5f0822d7f59890f70e1761
admin <yaz.volkov@gmail.com>
2025-07-13 09:40:16 +0300

    Now using TCPSocket

--- 06_networking/tcp_server.py
+++ 06_networking/tcp_server.py
@@ -1,22 +1,19 @@
-import socket
+from tcp_socket import TCPSocket
 
 class TCPServer:
     def __init__(self, host='localhost', port=8080):
         self.host = host
         self.port = port
-        self.server_socket = None
+        self.tcp_socket = TCPSocket(host, port)
 
     def start(self):
         """Start the TCP server."""
-        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # дозволяє перевикористання порту
-        self.server_socket.bind((self.host, self.port))
-        self.server_socket.listen(5)  # 50 занадто багато для простого тесту
-        print(f"Server started on {self.host}:{self.port}")
+        self.tcp_socket.set_reuse_addr()
+        self.tcp_socket.bind()
+        self.tcp_socket.listen(5)
         
-        while True:  # основний цикл сервера
-            client_socket, address = self.server_socket.accept()
-            print(f"Connection from {address}")
+        while True:
+            client_socket = self.tcp_socket.accept()
             self.handle_client_echo(client_socket)
 
     def handle_client_echo(self, client_socket):
@@ -32,12 +29,12 @@ class TCPServer:
 
     def stop(self):
         """Stop the TCP server."""
-        if self.server_socket:
-            self.server_socket.close()
+        if self.tcp_socket:
+            self.tcp_socket.close()
             print("Server stopped")
 
 if __name__ == "__main__":
-    server = TCPServer()
+    server = TCPServer("localhost", 8081)
     try:
         server.start()
     except KeyboardInterrupt:
